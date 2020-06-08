bc6d7f14bc385d7b65bd6b2196ea5d1dc3b32f05
Mário Wessen <mariowessen@gmail.com>
2020-06-08 20:46:02 -0300

    desafio nodejs

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,50 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url , techs } = request.body;
+  const repositorie = { id: uuid(), title, url, techs, likes: 0 };
+  repositories.push(repositorie);
+  response.json(repositorie);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { url, title,  techs } = request.body;
+  const index = repositories.map(repo => repo.id).indexOf(id);
+  if(index<0) return response.status(400).send();
+  if(url || title || techs) {
+    url ? repositories[index].url = url : repositories
+    title ? repositories[index].title = title : repositories
+    techs ? repositories[index].techs = techs : repositories
+  }
+  return response.json(repositories[index]);    
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = repositories.map(repo => repo.id).indexOf(id);
+  if(index<0){
+    response.status(400).send();
+  } else{
+    repositories.splice(index,1);
+    response.status(204).send();
+  }
+  
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = repositories.map(repo => repo.id).indexOf(id);
+  if(index<0){
+    response.status(400).send();
+  } else {
+    repositories[index].likes++;
+    response.json({ "likes": repositories[index].likes });
+  }    
 });
 
 module.exports = app;
